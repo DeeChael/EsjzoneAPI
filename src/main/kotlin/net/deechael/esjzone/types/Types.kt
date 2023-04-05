f34bc05e32c32955165ab1b693761f08118af15f
DeeChael <DeeChael@outlook.com>
2023-04-05 23:52:53 +0800

    feat: two enums for searching

---
+++ src/main/kotlin/net/deechael/esjzone/types/Types.kt
@@ -0,0 +1,14 @@
+package net.deechael.esjzone.types
+
+enum class Types(val index: Int) {
+
+    ALL(0), JAPANESE(1), ORIGINAL(2), KOREAN(3)
+
+}
+
+enum class Sorts(val index: Int) {
+
+    RECENTLY_UPDATED(1), RECENTLY_CREATED(2), HIGHEST_RANK(3), MOST_VIEWS(4),
+    MOST_CHAPTERS(5), MOST_COMMENTS(6), MOST_FAVORITES(7), MOST_WORDS(8)
+
+}
